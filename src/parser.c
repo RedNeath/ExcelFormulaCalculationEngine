d01bea3e5bccc0e8a5c0685db3486537a34f13c2
RedNeath <theandre.bourry@ik.me>
2024-06-05 17:21:53 +0200

    [WIP] Descending recursion parsing (grammar corrected)

--- src/parser.c
+++ src/parser.c
@@ -3,3 +3,90 @@
 //
 
 #include "parser.h"
+
+formula_token *parse_formula(formula_context *context, char *input) {
+	if (*input != '=') {
+		return &leaf_token;
+	}
+
+	input++;
+	return parse_expression(context, input); // Potentially more than just one element in there!
+}
+
+formula_token *parse_expression(formula_context *context, char *input) {
+    formula_token *token;
+    if (strlen(input) == 0) return &leaf_token;
+
+    token = parse_value(context, input);
+    if (token->type != TYPE_NONE) return token;
+
+    token = parse_function(context, input);
+    if (token->type != TYPE_NONE) return token;
+
+    token = parse_comparison(context, input);
+    if (token->type != TYPE_NONE) return token;
+
+    token = parse_concatenation(context, input);
+    if (token->type != TYPE_NONE) return token;
+
+    token = parse_addition_or_substraction(context, input);
+    if (token->type != TYPE_NONE) return token;
+
+    token = parse_multiplication_or_division(context, input);
+    if (token->type != TYPE_NONE) return token;
+
+    token = parse_power(context, input);
+    if (token->type != TYPE_NONE) return token;
+
+    token = parse_percent(context, input);
+    if (token->type != TYPE_NONE) return token;
+
+    token = parse_negation(context, input);
+    if (token->type != TYPE_NONE) return token;
+
+    token = parse_parenthesis(context, input);
+    if (token->type != TYPE_NONE) return token;
+
+    // Variable evaluation last, because this might be the slowest to evaluate (especially if not a variable)
+    token = parse_variable(context, input);
+    if (token != TYPE_NONE) return token;
+
+    return &leaf_token;
+}
+
+formula_token *parse_parenthesis(formula_context *context, char *input) {
+    if (*input != '(' || input[strlen(input) - 1] != ')') return &leaf_token;
+
+    input++;
+    input[strlen(input) - 1] = '\0';
+
+    return parse_expression(context, input);
+}
+
+formula_token *parse_negation(formula_context *context, char *input) {
+    if (*input != '-') return &leaf_token;
+
+    input++;
+    formula_token *token = malloc(sizeof(formula_token));
+    token->value = "-";
+    token->type = TYPE_UNARY_OPERATOR;
+    token->children = parse_expression(context, input);
+
+    return token;
+}
+
+formula_token *parse_percent(formula_context *context, char *input) {
+    if (input[strlen(input) - 1] != '%') return &leaf_token;
+
+    input[strlen(input) -1] = '\0';
+    formula_token *token = malloc(sizeof(formula_token));
+    token->value = "%";
+    token->type = TYPE_UNARY_OPERATOR;
+    token->children = parse_expression(context, input);
+
+    return token;
+}
+
+formula_token *parse_power(formula_context *context, char *input) {
+
+}
--- src/parser.h
+++ src/parser.h
@@ -14,28 +14,36 @@
 #define TYPE_FUNCTION 4
 #define TYPE_FUNCTION_ARGS 5
 #define TYPE_FUNTCTION_NAME 6
-#define TYPE_OPERATOR 7
-#define TYPE_SPECIAL_CHARACTER 8
-#define TYPE_VARIABLE 9
-#define TYPE_VALUE 10
+#define TYPE_PARENTHESIS 7
+#define TYPE_BINARY_OPERATOR 8
+#define TYPE_UNARY_OPERATOR 9
+#define TYPE_SPECIAL_CHARACTER 10
+#define TYPE_VARIABLE 11
+#define TYPE_VALUE 12
 
 typedef struct formula_token formula_token;
 
 struct formula_token {
     char *value;
     unsigned short type;
-    formula_token *child;
+    formula_token *children;
 };
 
-extern formula_token parse_formula(formula_context *context, char *input);
-extern formula_token parse_expression(formula_context *context, char *input);
-extern formula_token parse_operand(formula_context *context, char *input);
-extern formula_token parse_function(formula_context *context, char *input);
-extern formula_token parse_function_args(formula_context *context, char *input);
-extern formula_token parse_function_name(formula_context *context, char *input);
-extern formula_token parse_operator(formula_context *context, char *input);
-extern formula_token parse_special_character(formula_context *context, char *input);
-extern formula_token parse_variable(formula_context *context, char *input);
-extern formula_token parse_value(formula_context *context, char *input);
+static const formula_token leaf_token = { NULL, 0, NULL };
+
+extern formula_token *parse_formula(formula_context *context, char *input);
+extern formula_token *parse_expression(formula_context *context, char *input);
+extern formula_token *parse_parenthesis(formula_context *context, char *input);
+extern formula_token *parse_negation(formula_context *context, char *input);
+extern formula_token *parse_percent(formula_context *context, char *input);
+extern formula_token *parse_power(formula_context *context, char *input);
+extern formula_token *parse_multiplication_or_division(formula_context *context, char *input);
+extern formula_token *parse_addition_or_substraction(formula_context *context, char *input);
+extern formula_token *parse_concatenation(formula_context *context, char *input);
+extern formula_token *parse_comparison(formula_context *context, char *input);
+extern formula_token *parse_function(formula_context *context, char *input);
+extern formula_token *parse_variable(formula_context *context, char *input);
+extern formula_token *parse_value(formula_context *context, char *input);
+extern formula_token *parse_string(formula_context *context, char *input);
 
 #endif //PARSER_H
